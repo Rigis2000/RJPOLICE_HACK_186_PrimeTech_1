f3797af9229c52715015b9f9b24be5da552933a6
Rigis2000 <130860289+Rigis2000@users.noreply.github.com>
2024-01-17 22:55:07 +0530

    Update Bulk_SMS.py

--- oneclick/Bulk_SMS.py
+++ oneclick/Bulk_SMS.py
@@ -1,20 +1,20 @@
 from twilio.rest import Client
 
 account_sid = 'AC590bcb984ba74908db827d06c0d639cd'
-auth_token = '3facfa87a2403fe48d761d8eeb81ff68'
+auth_token = 'e6a999779a456d5466d3adaf9148e092'
 
 twilio_phone_number = '+12059316077'
 
-recipient_phone_number = '+919462205207'
+recipient_phone_number = ['+919462205207',"+916377957714"]
 
-link_url = 'https://rajasthanpolicefeedback.000webhostapp.com/home.html'
+link_url = 'http://10.255.2.163/PFS_Final/oneclick/oneclickfeedback.html'
 
 client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-    body=f'Click the link to give feedback: {link_url}',
-    from_=twilio_phone_number,
-    to=recipient_phone_number
-)
+for number in recipient_phone_number:
+    message = client.messages.create(
+        body=f'Click the link to give feedback: {link_url}',
+        from_=twilio_phone_number,
+        to=number
+    )
 
 print(f'SMS sent successfully. Message SID: {message.sid}')
